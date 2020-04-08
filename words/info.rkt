a6bd3f7086182be24593ef6ea5b5c0e71ec5f7cc
Matthew Butterick <mb@mbtype.com>
2020-04-08 06:52:23 -0700

    stub launcher

--- words/info.rkt
+++ words/info.rkt
@@ -2,3 +2,6 @@
 
 (define post-install-collection "index.rkt")
 (define raco-commands '(("words" (submod words/command raco) "issue words command" #f)))
+
+#;(define racket-launcher-names '("Words.app"))
+#;(define racket-launcher-libraries '("app.rkt"))
