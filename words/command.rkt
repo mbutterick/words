64a404fa305aaac2027bc28d5af748f5bdcc1032
Matthew Butterick <mb@mbtype.com>
2020-04-09 21:37:03 -0700

    sorting etc

--- words/command.rkt
+++ words/command.rkt
@@ -1,6 +1,7 @@
 #lang debug racket
 (require racket/string
-         "main.rkt")
+         "main.rkt"
+         "kernscore.rkt")
 
 (module+ raco
   (define command-name (with-handlers ([exn:fail? (λ (exn) #f)])
@@ -19,6 +20,11 @@
   (define count 20)
   (define hide-plurals #true)
   (define proper-names #false)
+  (define title-case #false)
+  (define uppercase #false)
+  (define sort-style #false)
+  (define omit #false)
+  (define reversed #false)
   (command-line
    #:program "words"
    #:argv (current-command-line-arguments)
@@ -29,6 +35,10 @@
    [("-m" "--mandatory") mandatory-arg
                          "mandatory letters"
                          (set! mandatory mandatory-arg)]
+  [("-o" "--omit") omit-arg
+                         "forbidden letters"
+                         (set! omit omit-arg)]
+   
    [("-c" "--combo") combo-arg
                      "mandatory combo"
                      (set! combo combo-arg)]
@@ -42,18 +52,44 @@
               "maximum word length"
               (set! max-size (string->number max-size-arg))]
    [("-s" "--show-plurals") 
-                      "show plural words"
-                      (set! hide-plurals #false)]
+    "show plural words"
+    (set! hide-plurals #false)]
    [("-p" "--proper-names") 
-                      "show proper names"
-                      (set! proper-names #true)])
-  (displayln (string-join (make-words #:letters letters
-                                      #:mandatory mandatory
-                                      #:combo combo
-                                      #:count count
-                                      #:min min-size
-                                      #:max max-size
-                                      #:hide-plurals hide-plurals
-                                      #:proper-names proper-names) "\n")))
+    "show proper names"
+    (set! proper-names #true)]
+   [("--sort") sort-arg
+               "sort order"
+               (set! sort-style sort-arg)]
+   [("-r" "--reverse") 
+               "reverse order"
+               (set! reversed #true)]
+   #:once-any
+   [("-t" "--title-case") 
+    "capitalize first letter"
+    (set! title-case #true)]
+   [("-u" "--uppercase") 
+    "capitalize all letters"
+    (set! uppercase #true)])
+  (define words (make-words #:letters letters
+                            #:mandatory mandatory
+                            #:omit omit
+                            #:combo combo
+                            #:count count
+                            #:min min-size
+                            #:max max-size
+                            #:hide-plurals hide-plurals
+                            #:proper-names proper-names
+                            #:all-caps uppercase
+                            #:initial-caps title-case))
+  (define-values (sort-key sort-cmp)
+    (match sort-style
+      ["kernscore" (values kernscore <)]
+      ["alpha" (values values string<=?)]
+      [_ (values #false #false)]))
+  (define sorted-words (cond
+                         [sort-key
+                          (sort words sort-cmp #:key sort-key #:cache-keys? #true)]
+                         [else words]))
+  (displayln (string-join ((if reversed reverse values) sorted-words) "\n")))
 
 
