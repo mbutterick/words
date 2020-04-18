1ef848c4ca58792a6d7fbd312bf5a545a4f7fea2
Matthew Butterick <mb@mbtype.com>
2020-04-17 21:00:43 -0700

    misconfig

--- words/info.rkt
+++ words/info.rkt
@@ -3,5 +3,5 @@
 (define post-install-collection "index.rkt")
 (define raco-commands '(("words" (submod words/command raco) "issue words command" #f)))
 
-(define gracket-launcher-names '("Words.app"))
+(define gracket-launcher-names '("Words"))
 (define gracket-launcher-libraries '("app.rkt"))
